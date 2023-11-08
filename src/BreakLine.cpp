285591d867af1cc73a58b34fbe7a788660e8cafc
Richard <legalize@xmission.com>
2023-11-08 10:36:13 -0700

    Hard line break when no whitespace present

--- src/BreakLine.cpp
+++ src/BreakLine.cpp
@@ -2,12 +2,22 @@
 
 #include <boost/algorithm/string/trim.hpp>
 
+#include <string>
+
 namespace stringAlgos
 {
 
+constexpr size_t LINE_LENGTH = 80;
+
 void breakLine(std::ostream &output, std::string_view input)
 {
-    output << boost::algorithm::trim_copy(input);
+    std::string line{boost::algorithm::trim_copy(input)};
+    while (line.length() > LINE_LENGTH)
+    {
+        output << line.substr(0, LINE_LENGTH) << "\n    ";
+        line = line.substr(LINE_LENGTH);
+    }
+    output << line;
 }
 
 } // namespace stringAlgos
--- tests/TestBreakLine.cpp
+++ tests/TestBreakLine.cpp
@@ -2,24 +2,76 @@
 
 #include <gtest/gtest.h>
 
+#include <iterator>
 #include <sstream>
+
+namespace
+{
+
+template <size_t N>
+std::string repeat(const char (&repeaters)[N])
+{
+    std::string line;
+    for (char c : repeaters)
+    {
+        if (c > 0)
+        {
+            line += std::string(10, c);
+        }
+    }
+    return line;
+}
+
+} // namespace
+
+TEST(TestRepeat, count)
+{
+    const std::string line{repeat("0123")};
+
+    EXPECT_EQ(40U, line.size());
+}
 
 class TestBreakLine : public testing::Test
 {
 protected:
-    std::ostringstream output;
+    std::ostringstream m_output;
 };
 
 TEST_F(TestBreakLine, emptyLineUnchanged)
 {
-    stringAlgos::breakLine(output, "");
+    stringAlgos::breakLine(m_output, "");
 
-    EXPECT_TRUE(output.str().empty());
+    EXPECT_TRUE(m_output.str().empty());
 }
 
 TEST_F(TestBreakLine, leadingHorizontalWhiteSpaceDropped)
 {
-    stringAlgos::breakLine(output, " \tLeading horizontal white space.");
+    stringAlgos::breakLine(m_output, " \tLeading horizontal white space.");
 
-    EXPECT_EQ(output.str(), "Leading horizontal white space.");
+    EXPECT_EQ(m_output.str(), "Leading horizontal white space.");
+}
+
+TEST_F(TestBreakLine, trailingHorizontalWhiteSpaceDropped)
+{
+    stringAlgos::breakLine(m_output, "Trailing horizontal white space.\t ");
+
+    EXPECT_EQ(m_output.str(), "Trailing horizontal white space.");
+}
+
+TEST_F(TestBreakLine, longLinesWithoutWhiteSpaceAreHardFolded)
+{
+    std::string line{repeat("012345678")};
+
+    stringAlgos::breakLine(m_output, line);
+
+    EXPECT_EQ(repeat("01234567") + "\n    " + repeat("8"), m_output.str());
+}
+
+TEST_F(TestBreakLine, lineWithoutWhiteSpaceExactlyLineLengthNotFolded)
+{
+    std::string line{repeat("01234567")};
+
+    stringAlgos::breakLine(m_output, line);
+
+    EXPECT_EQ(line, m_output.str());
 }
